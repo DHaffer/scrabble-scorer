705a6ee955359a46d4ccde803ecc28d12112980c
Daryn Haffer <dhaffer1991@gmail.com>
2021-02-10 19:32:07 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,83 @@
+// Refer to the online book to access detailed instructions for this project.
+const input = require('readline-sync');
+// URL = https://education.launchcode.org/intro-to-professional-web-dev/assignments/scrabble-scorer.html
+
+// Code your transform function here:
+function transform(obj) {
+    let retobj = {};
+    for (key in obj) {
+        let arr = obj[key];
+        for (let i = 0; i < arr.length; i++) {
+            retobj[arr[i].toLowerCase()] = key;
+        }
+    }
+    return retobj;
+}
+
+
+// Code your initialPrompt function here:
+function initialPrompt() {
+let info = input.question(`
+Welcome to the Scrabble score calculator!
+
+Which scoring algorithm would you like to use?
+
+0 - Scrabble: The traditional scoring algorithm.
+1 - Simple Score: Each letter is worth 1 point.
+2 - Bonus Vowels: Vowels are worth 3 pts, and consonants are 1 pt.
+
+Enter 0, 1, or 2:`);
+return info;
+}
+
+// Code your runProgram function here:
+
+
+// Here is the oldPointStructure object:
+const oldPointStructure = {
+   1: ['A', 'E', 'I', 'O', 'U', 'L', 'N', 'R', 'S', 'T'],
+   2: ['D', 'G'],
+   3: ['B', 'C', 'M', 'P'],
+   4: ['F', 'H', 'V', 'W', 'Y'],
+   5: ['K'],
+   8: ['J', 'X'],
+   10: ['Q', 'Z']
+};
+
+// Use the transform function to create the newPointStructure object here:
+let newPointStructure = transform(oldPointStructure);
+console.log(newPointStructure)
+// Create your scoringAlgorithms array here:
+function scrabble(word, obj){
+let score = 0;
+for (let i = 0; i < word.length; i++){
+  for(key in obj){
+    if (word[i].toLowerCase() == key) {
+      score = score + Number(obj[key]);
+    }
+  }
+}
+return score;
+}
+scrabble('daryn', newPointStructure)
+
+
+function simpleScore(word){
+ let score = word.length;
+ return score;
+};
+
+function bonusVowels(word){
+let score = 0;
+for (let i = 0; i < word.length; i++){
+  if (/[aeiou]/.test(word[i].toLowerCase()) === true){
+    score = score + 3;
+  } else {
+    score++
+  }
+}
+return score;
+}
+
+
+// Call the runProgram function here:
